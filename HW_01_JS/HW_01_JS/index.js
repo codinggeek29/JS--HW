9e06a858013d43413625a15fc01d205d06838fbe
codinggeek29 <166082786+codinggeek29@users.noreply.github.com>
2024-04-30 17:54:54 -0400

    Delete HW_01_JS/HW_01_JS directory

--- HW_01_JS/HW_01_JS/index.js
+++
@@ -1,5 +0,0 @@
-console.log(" welcome to this magic eight ball application");
-const first_name = "Louisa";
-const last_name = "Kalisilira";
-let question = "How can I master JavaScript?";
-console.log = ("Hello may name is ${first_name} ${last_name} and my question is: ${question}");
